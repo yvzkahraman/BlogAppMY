ed71a4312fd7a036746cd9c4359fd8c5b111a136
Yavuz Selim KAHRAMAN <yskahraman@gmail.com>
2023-11-01 22:28:26 +0300

    completed

--- BlogApp/Controllers/Admin/BlogController.cs
+++ BlogApp/Controllers/Admin/BlogController.cs
@@ -15,41 +15,23 @@ namespace BlogApp.Controllers.Admin
             this.context = context;
         }
 
+        //public IActionResult RedirectIndex()
+        //{
+        //    return RedirectToAction("Index", new
+        //    {
+        //        Title = "",
+        //        CreatedDate = DateTime.MinValue,
+        //    });
+        //}
+
 
         [HttpGet]
         public IActionResult Index(BlogSearchModel model)
         {
-            //return new TestResult().ExecuteResultAsync;
-            //()
+            ViewBag.SearchModel = model;
             ViewBag.Active = "Blog";
 
-
-
-            //if(model.CreatedDate == DateTime.MinValue && string.IsNullOrWhiteSpace(model.Title))
-            //{
-            //    return View();
-            //}
-
-            // - bir sayfada kaç tane gözüksün.
-            //  hangi sayfadayız ??
-            //  kaç sayfam var.
-
-
-            // 100 =>  8 tane gözüksün
-            // 2 sayfadayım. ilk kaydı => 11
-            // 
-
-
-            // PageSize
-            // ActivePage
-            // Total
-
-
-
-
-
             var query = this.context.Blogs.AsQueryable();
-
 
             if (model.CreatedDate != DateTime.MinValue)
             {
@@ -60,21 +42,7 @@ namespace BlogApp.Controllers.Admin
             {
 
                 query = query.Where(x => x.Title.Contains(model.Title));
-            }
-
-
-            // Bir sayfada kaç tane gözüksün => 5: 
-            // Hangi sayafadayım ?? 
-
-            //1.sayfada isem => 1 2 3 4 5  => 5            1 2 3 4 5 6 7 8 9 10 .... 50
-            //2. sayfada isem=>6 7 8 9 10 => 5
-
-            //...............        . 48   
-
-            // 0 kayıt geç, 5 kayıt çek => 1. sayfa => ActivePage 
-            //5 kayıt geç, 5 kayıt çek => 2. sayfa
-
-
+            }       
 
             ViewBag.PageModel = new PageModel
             {
@@ -88,26 +56,48 @@ namespace BlogApp.Controllers.Admin
 
 
             var blogs = query.Skip((model.ActivePage - 1) * model.PageSize).Take(model.PageSize).ToList();
-
-            /* 5 kayıtı nasıl geçiyor
-             Acaba ilk 5 kayıdı hiç okumadan 2.sayfa için  5 kayıt mı seçiyor
-             ilk 5 kaydıda okuyor mu ?
-
-
-            1000 adet kaydımız var 
-            1 sayfada 50 adet gösteriyoruz.
-            300. kaydım, 50 tane kayıdımı getirir ? 300 tane kayıdımı getirir 1000 tane kaydımı getirir.
-
-            6. sayfanın.
-
-
-            
-            */
-
-
-
-
             return View(blogs);
+        }
+
+
+        [HttpGet]
+        public IActionResult AssignCategory(int id)
+        {
+            var list = new List<AssignCategoryListModel>();
+
+            var allCategories = this.context.Categories.AsNoTracking().ToList();
+            
+            
+            var filtredCategories = this.context.Categories.Join(this.context.BlogCategories, category=>category.Id, blog=>blog.CategoryId, (category, categoryBlog) =>new
+            {
+                category,
+                categoryBlog,
+            }).Where(x=>x.categoryBlog.BlogId == id).Select(x=>new Category
+            {
+                Definition = x.category.Definition,
+                Id = x.category.Id,
+                SeoUrl = x.category.SeoUrl,
+            }).AsNoTracking().ToList();
+
+
+            foreach (var category  in allCategories)
+            {
+                bool exist = false;
+                if (filtredCategories.Contains(category))
+                {
+                    exist = true;   
+                }
+                list.Add(new AssignCategoryListModel
+                {
+                    BlogId = id,
+                    Definition = category.Definition,
+                    Id = category.Id,
+                    Exist = exist
+                });
+            }
+
+
+            return View(list);
         }
 
     }
