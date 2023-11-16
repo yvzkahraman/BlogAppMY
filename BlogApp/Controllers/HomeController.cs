4d00cc59a64ab69cfea784aee426f13ff2844112
Yavuz Selim KAHRAMAN <yskahraman@gmail.com>
2023-11-17 02:40:46 +0300

    jquery-ajax eklendi

--- BlogApp/Controllers/HomeController.cs
+++ BlogApp/Controllers/HomeController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlogApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly BlogDbContext context;
+
+        public HomeController(BlogDbContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetCategories()
+        {
+            Thread.Sleep(3000);
+
+            var categories = this.context.Categories.AsNoTracking().ToList();
+
+            return Json(categories);
         }
     }
 }
